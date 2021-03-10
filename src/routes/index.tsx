98242174fb9cb921f9087237912f08a810795d79
diipacheco <edilsonpacheco6@gmail.com>
2021-03-10 07:14:35 -0300

    Adiociona rotas autenticadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={Signin} />
       <Route path="/signup" exact component={Signup} />
+
+      <Route path="/dashboard" isPrivate component={Dashboard} />
     </Switch>
   );
 };
