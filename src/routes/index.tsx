5599e11959c06771455ef111ba975ca49bd623a3
diipacheco <edilsonpacheco6@gmail.com>
2021-05-07 22:38:33 -0300

    Add Forgot Password screen and implementation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,12 +6,14 @@ import Route from './Route';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Dashboard from '../pages/Dashboard';
+import ForgotPassword from '../pages/ForgotPassword';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={Signin} />
       <Route path="/signup" exact component={Signup} />
+      <Route path="/forgot-password" exact component={ForgotPassword} />
 
       <Route path="/dashboard" isPrivate component={Dashboard} />
     </Switch>
